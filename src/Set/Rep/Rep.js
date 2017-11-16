373403cc1588d06ae16ebe9da553b4ff5d80fd18
Alex Jegtnes <alex@jegtnes.com>
2017-11-16 01:10:32 +0000

    I am trash

--- src/Set/Rep/Rep.js
+++ src/Set/Rep/Rep.js
@@ -5,64 +5,75 @@ import { round } from '../../services/round';
 
 class Rep extends Component {
   recursive(weights, totalWeight, previousResult) {
+    weights = weights.reverse();
     if (totalWeight < 0) {
+      // console.log(totalWeight, previousResult);
+      console.log('feck', previousResult, totalWeight);
       return previousResult;
     }
     if (totalWeight === 0) {
-      console.log(previousResult)
       return previousResult;
     }
-    if (weights[0] < totalWeight) {
-      previousResult.push(weights[0]);
-      return this.recursive(weights, totalWeight - weights[0], previousResult);
-    } else {
-      var biggestWeight = 0;
 
-      for(var i = weights.length; i > -1; i--) {
-        if (weights[i] < totalWeight) {
-          biggestWeight = weights[i-1];
-        }
+    var biggestWeight = 0;
+
+    // for(var i = weights.length - 1; i >= 0; i--) {
+    //   if (weights[i] < totalWeight) {
+    //     console.info(biggestWeight, weights[i]);
+    //     biggestWeight = weights[i];
+    //   }
+    // }
+
+    console.log('loop start');
+    for(var i = 0; i < weights.length; i++) {
+      console.info(biggestWeight, weights[i]);
+      if (weights[i] <= totalWeight) {
+        biggestWeight = weights[i];
       }
-
-      previousResult.push(biggestWeight);
-
-      return this.recursive(weights, totalWeight - biggestWeight, previousResult);
     }
+    console.log('loop end');
+
+    if (biggestWeight === 0) return previousResult;
+
+    previousResult.push(biggestWeight);
+
+    console.error('biggest weight', biggestWeight, 'total', totalWeight, 'total weight minus biggest weight', totalWeight - biggestWeight);
+
+    return this.recursive(weights.reverse(), totalWeight - biggestWeight, previousResult);
+
   }
 
   calculateWeights(weight, totalWeight, previousResult) {
+    console.warn(totalWeight);
     var weights = this.recursive(weight, totalWeight, previousResult);
-    console.log(weights);
-    weights = weights.join(' + ');
-    console.log('tada', weights);
+    weights = weights.join('+');
     return weights;
   }
 
   render() {
+    const weight = round(
+      this.props.orm * generateRepIntensity(
+        this.props.weekNumber,
+        this.props.setNumber,
+        this.props.roundingFactor ? this.props.roundingFactor : 2.5,
+      )
+    );
+
     return (
-      <div>
+      <li>
         <span className="lifts">{this.props.liftNumber}&nbsp;</span>
         <span className="weight">
-          {round(this.props.orm *
-            generateRepIntensity(
-              this.props.weekNumber, this.props.setNumber),
-              this.props.roundingFactor ? this.props.roundingFactor : 2.5,
-            )}
+          {weight}
           {this.props.unit}
         </span>
-        <span class="weight-distribution">{
+        <span className="weight-distribution">{
           this.calculateWeights(
               [1.25, 2.5, 5, 10, 15, 20, 25, 30].reverse(),
-              // generateRepIntensity(
-              //   this.props.weekNumber,
-              //   this.props.setNumber,
-              //   this.props.roundingFactor ? this.props.roundingFactor : 2.5,
-              // ),
-              50,
+              parseFloat(((weight - 20) / 2)),
               []
           )
-        }/each side</span>
-      </div>
+        }/side</span>
+      </li>
     )
   }
 }
